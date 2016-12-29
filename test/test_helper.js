207d61c0fe1c8dfb6dcfe4441152732d37ffd666
Qing Zhou <qing.zhou@rea-group.com>
2016-12-29 08:17:06 +0800

    Replace mongoose default promise to ES6 Promise

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection.once('open', () => {
-  console.log('Good to go!');
-}).on('error', (error) => {
-  console.log('Error' + error);
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection.once('open', () => {done();})
+                     .on('error', (error) => {
+                          console.log('Error' + error);
+                     });
 });
+
 
 
 beforeEach((done) => {
