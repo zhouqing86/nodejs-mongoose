096f5ebb09d87421e3d3ff25820296c47b5e184e
Qing Zhou <qing.zhou@rea-group.com>
2016-12-31 10:14:07 +0800

    Add pagination support by skip and limit

--- test/reading_test.js
+++ test/reading_test.js
@@ -4,11 +4,14 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading Users', () => {
-  let joe;
+  let joe, alex, maria, zach;
 
   beforeEach((done) => {
     joe = new User({name : 'Joe'});
-    joe.save().then(() => done());
+    alex = new User({name: 'Alex'});
+    maria = new User({name: 'Maria'});
+    zach = new User({name: 'Zach'});
+    Promise.all([joe.save(), alex.save(), maria.save(), zach.save()]).then(() => done());
   });
 
   it('Find all users with a name joe', (done) => {
@@ -16,5 +19,22 @@ describe('Reading Users', () => {
       assert(users[0]._id.toString() == joe._id.toString()); //_id is ObjectId, so can't compare directly.
       done();
     });
+  });
+
+  it.only('Can skip and limit the result', (done) => {
+    User.find({})
+      .sort({ name: 1})
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length == 2);
+        assert(users[0].name == 'Joe');
+        assert(users[1].name == 'Maria');
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
   });
 });
